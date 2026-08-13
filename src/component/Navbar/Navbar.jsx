b9d18bd032de3ce0f5932536447f7d3e5013cd8a
dhumal sanika <dhumalsanika34@gmail.com>
2026-08-13 21:43:46 +0530

    adding the navbar in login and registretion

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import "./Navbar.css";
 function Navbar() {
   return (
+
     <nav className="navbar">
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
+      <div className="navbar-logo">
+        <img src="https://via.placeholder.com/100x40?text=Logo" alt="Logo" />
+      </div>
+      <div className="navbar-links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div className="navbar-auth">
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+      </div>
     </nav>
+
   );
 }
 export default Navbar;
