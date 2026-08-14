4f6f1dabf0fff12545783bf70776cdc350f1ec42
dhumal sanika <dhumalsanika34@gmail.com>
2026-08-14 16:52:49 +0530

    adding the card code of movie

---
+++ src/component/MovieCard/MovieCard.jsx
@@ -0,0 +1,11 @@
+function MovieCard() {
+    return (
+        <div className="movie-card">
+            <img src="movie-poster.jpg" alt="Movie Poster" />
+            <h2>Movie Title</h2>
+            <p>Movie description goes here. It provides a brief overview of the movie plot and key details.</p>
+            <button>Watch Now</button>
+        </div>
+    );
+}
+export default MovieCard;
